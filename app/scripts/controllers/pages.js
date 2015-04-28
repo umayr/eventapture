8a07ce2234d0fe8f3eeb6966ae24861cd699e8c2
Umayr Shahid <umayrr@hotmail.co.uk>
2015-04-28 07:54:08 +0500

    added complete pages feature alongwith minor style fixes and number crunching sidebar

--- app/scripts/controllers/pages.js
+++ app/scripts/controllers/pages.js
@@ -14,5 +14,77 @@
     /*jshint validthis: true */
     var vm = this;
     vm.hello = 'hello, pages';
+
+    vm.options = {
+      chart: {
+        type: 'pieChart',
+        height: 320,
+        donut: true,
+        x: function (d) {
+          return d.key;
+        },
+        y: function (d) {
+          return d.y;
+        },
+        showLabels: false,
+
+        pie: {
+          startAngle: function (d) {
+            return d.startAngle - Math.PI
+          },
+          endAngle: function (d) {
+            return d.endAngle - Math.PI
+          }
+        },
+        transitionDuration: 500,
+        legend: {
+          margin: {
+            top: 18,
+            right: 10,
+            bottom: 5,
+            left: 0
+          }
+        }
+      }
+    };
+
+
+    vm.makeData = function () {
+      return [
+        {
+          key: "One",
+          value: Math.random()
+        },
+        {
+          key: "Two",
+          value: Math.random()
+        },
+        {
+          key: "Three",
+          value: Math.random()
+        },
+        {
+          key: "Four",
+          value: Math.random()
+        }
+      ];
+    };
+    vm.data = [vm.makeData(), vm.makeData(), vm.makeData()];
+
+    vm.makeRandom = function () {
+      return Math.floor(Math.random() * 400) + 10;
+    };
+    vm.categories = [
+      { name : 'Sports', count : vm.makeRandom()},
+      { name : 'Politics', count : vm.makeRandom()},
+      { name : 'Travel', count : vm.makeRandom()},
+      { name : 'Celebrities', count : vm.makeRandom()},
+      { name : 'Movies', count : vm.makeRandom()},
+      { name : 'Religion', count : vm.makeRandom()}
+    ];
+
+
+vm.showMoreInfo = false;
+    vm.showSidebar = false;
   }
 })();
